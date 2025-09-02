f1a609e6dd02cff180c7c229f20e85ab90921b35
Kumar Kaustubh <86567639+krkaustubh@users.noreply.github.com>
2025-09-02 23:21:07 +0530

    Create Nested_IF_ELSE.py

---
+++ AssignmentDay2_29_Aug_2025/Nested_IF_ELSE.py
@@ -0,0 +1,15 @@
+#Assignment 2 Day2
+score = 90
+if score > 90:
+    print("Grade A")
+else:
+    if score > 80:
+        print("Grade B")
+    else:
+        if score > 70:
+            print("Grade C")
+        else:
+            if score > 60:
+                print("Grade D")
+            else:
+                print("Grade F")
